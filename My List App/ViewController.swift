ea8f67bab1b2621748e1d71c14a593774196b02b
michaeladudarko <adudarkomichael@gmail.com>
2019-10-17 20:33:50 -0400

    a view is set on the main UiView

--- My List App/ViewController.swift
+++ My List App/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let bg:UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        view.addSubview(bg)
+        bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        
     }
 
 
