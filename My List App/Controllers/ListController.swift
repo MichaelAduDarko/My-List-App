a476e03613a594969f3d0616efcfedde3e170a88
michaeladudarko <adudarkomichael@gmail.com>
2019-11-01 20:17:23 -0400

    added a table view

--- My List App/Controllers/ListController.swift
+++ My List App/Controllers/ListController.swift
@@ -8,18 +8,28 @@
 
             import UIKit
 
-            class ListController: UIViewController, headerDelegate, NewItemDelegate {
-                func openAddItemPopup() {
-                    print("add item open")
-                }
-                
-                func addItemToList(text:String) {
-                    print("Text in textfield is : \(text)")
-                }
-                
-                let header = AppHeaderView(title: "Stuff to get done", subtitle: "4 left")
-                let popUp = NewItemPopUp()
-                
+class ListController: UIViewController, headerDelegate, NewItemDelegate {
+               func openAddItemPopup() {
+                            print("add item open")
+                        }
+    
+                        func addItemToList(text:String) {
+                            print("Text in textfield is : \(text)")
+                        }
+    
+                            let header = AppHeaderView(title: "Stuff to get done", subtitle: "4 left")
+                            let popUp = NewItemPopUp()
+    
+                            let bg:UIView = {
+                                let view = MyListAppGradient()
+                                view.layer.cornerRadius = 24
+                                view.translatesAutoresizingMaskIntoConstraints = false
+                                return view
+                            }()
+            let lisTable = tableView()
+    
+    
+    
                 var keyboardHeight: CGFloat = 345
 
                 
@@ -44,23 +54,34 @@
                         
                     }
 
-            
-            
-            
-            
-            
-            
+                
+                
             override func viewDidLoad() {
                 super.viewDidLoad()
+                
                 view.backgroundColor = .white
-          
-            
+                
+           
                     view.addSubview(header)
                     header.translatesAutoresizingMaskIntoConstraints = false
                     header.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
                     header.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
                     header.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
                     header.heightAnchor.constraint(equalToConstant: 120).isActive = true
+                
+                view.addSubview(bg)
+                bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+                bg.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 20).isActive = true
+                bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+                bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+                
+                view.addSubview(lisTable)
+                lisTable.leftAnchor.constraint(equalTo: bg.leftAnchor, constant: 8).isActive = true
+                lisTable.topAnchor.constraint(equalTo: bg.topAnchor, constant: 8).isActive = true
+                lisTable.bottomAnchor.constraint(equalTo: bg.bottomAnchor, constant: -8).isActive = true
+                lisTable.rightAnchor.constraint(equalTo: bg.rightAnchor, constant: -8).isActive = true
+                
+                
 
                     view.addSubview(popUp)
                     popUp.translatesAutoresizingMaskIntoConstraints = false
---
+++ My List App/custom/tableView.swift
@@ -0,0 +1,22 @@
+//
+//  tableView.swift
+//  My List App
+//
+//  Created by MICHAEL ADU DARKO on 11/1/19.
+//  Copyright © 2019 Bronzy. All rights reserved.
+//
+
+import UIKit
+
+class tableView: UITableView {
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        checkIfAutoLayOut()
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
